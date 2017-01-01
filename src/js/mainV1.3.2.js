d83b47aa96b68b4a27e04ddf69d8931b0701f84f
jacean <jaceanwan@gmail.com>
2017-01-01 21:06:50 +0800

    add:demo例子完善，利用loadwait來等待異步完成

--- src/js/mainV1.3.2.js
+++ src/js/mainV1.3.2.js
@@ -1,40 +1,27 @@
-var requireV1=require.config({
+var requireV1 = require.config({
+    baseUrl: "../../src/js",
     shim: {
         'jquery': {
             exports: "$"
+        },
+        'checkbox':{
+            deps:['jquery']
         }
-    },  
-    context:"context-v1",
+    },
+    context: "context-v1",
     paths: {
         "jquery": "lib/jquery-1.3.2",
-        "piechart": "core/piechart"
+        "piechart": "core/piechart",
+        "checkbox":"core/checkbox"
     }
 });
-var requireV3=require.config({
-    context:"context-v3",
+var requireV3 = require.config({
+    baseUrl: "../../src/js",
+    context: "context-v3",
     paths: {
         "jquery": "lib/jquery-3.1.1",
         "piechart": "core/piechart"
     }
 });
-requireV1(['jquery', 'piechart'], function ($, piechart) {
-    if (document.readyState == "complete") {//当页面加载状态 
-        $("#piechart").piechart({
-            width: 300,
-            height: 300,
-            dataArray: dataArray,
-            tooltip: true
-        });
-    } else {
-        $(document).ready(function () {
-            $("#piechart").piechart({
-                width: 300,
-                height: 300,
-                dataArray: dataArray,
-                tooltip: true
-            });
-        });
-    }
 
-});
 
